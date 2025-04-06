2c1fa23cace0407841f376c4d3f9c92aa6886410
Limonero <bigdikfast2132@gmail.com>
2025-04-06 17:24:30 -0300

    First functional Tauleph version 0.1.

---
+++ discord_obj_processor.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+class DiscordObjectProcessor:
+    def __init__(self):
+        self.message_author: str = ""
+        self.bot_name: str = ""
+        self.message_content: str = ""
+
+        self.message: discord.Message = None
+        self.guild: discord.Guild = None
+
+        self.attachments: discord.Message = ""
+        self.att_type: str = ""
+        self.att_url: str = ""
+
+    async def update_object_variables(self, discord_object: discord.Message, bot: commands.Bot):
+        """
+        Gets a list of names that contains the bot's name, the user's name, and the user's message.
+
+        Args:
+            discord_object (discord.Message/Interaction): The Discord object to take as reference.
+            is_interaction (bool): 
+        """
+        
+        self.guild = discord_object.guild
+        self.message_author = discord_object.author.display_name 
+        bot_member = await self.guild.fetch_member(bot.user.id)
+        self.bot_name = bot_member.display_name
+        self.message_content = discord_object.content
+        
+        
+        self.message = discord_object
+        
+        self.attachments = discord_object.attachments
+        self.att_type = self.attachments[-1].content_type if self.attachments else ""
+        self.att_url = self.attachments[-1].url if self.attachments else ""
+        
+discord_obj_processor = DiscordObjectProcessor()
+
---
+++ main.py
@@ -0,0 +1,24 @@
+from message_processor import MessageProcessor
+from checkpoint_manager import CheckpointManager
+
+
+
+checkpoint_manager = CheckpointManager()
+
+async def entry_point():
+    """
+    Entry point for processing messages.
+    
+    This method is called to start the message processing flow.
+    """
+    # Process the message.
+    message_processor = MessageProcessor()
+    result = await message_processor.process_message()
+
+    # Get the API response.
+    llm_output = await checkpoint_manager.response(result[0], result[1])
+
+    return llm_output
+
+
+    
